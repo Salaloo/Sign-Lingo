eaa9aef2e522b40f56fdc54e3050cd37302e9444
Salaloo <mbhuiyan8596@bths.edu>
2024-02-10 21:09:13 -0500

    flip

--- sign-lingo/src/App.jsx
+++ sign-lingo/src/App.jsx
@@ -2,8 +2,15 @@ import { useState } from 'react'
 import logo from '/SignLingo.png'
 
 import './App.css'
+import ReactCardFlip from 'react-card-flip'
 
 function App() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  function flipCard() {
+    setIsFlipped(!isFlipped);
+  }
+
   return (
     <><img src={logo} className="logo"></img>
       <h1 className= "heading1">Learn American Sign Language</h1>
@@ -13,6 +20,16 @@ function App() {
         <button className= "ABCs">Travel</button>
         <button className= "ABCs">Cooking</button>
         <button className= "ABCs">Sports</button>
+        <div>
+          <ReactCardFlip flipDirection='horizontal' isFlipped={isFlipped}>
+            <div className='card' onClick={flipCard}>
+              <h1>Front</h1>
+            </div>
+            <div className='card card-back' onClick={flipCard}>
+              <h1>Back</h1>
+            </div>
+          </ReactCardFlip>
+        </div>
     </>
   )
 }
