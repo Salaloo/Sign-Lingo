9ba0c6fa1776b6516519aeffecae7ed5e29b717f
NayeemK14628 <153224838+NayeemK14628@users.noreply.github.com>
2024-02-10 20:26:06 -0500

    everything

---
+++ sign-lingo/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import logo from '/SignLingo.png'
+
+import './App.css'
+
+function App() {
+  return (
+    <><img src={logo} className="logo"></img>
+      <h1 className= "heading1">Learn American Sign Language</h1>
+        <button className= "ABCs">ABCs</button>
+        <button className= "ABCs">Basic Words</button>
+        <button className= "ABCs">Colors</button>
+        <button className= "ABCs">Travel</button>
+        <button className= "ABCs">Cooking</button>
+        <button className= "ABCs">Sports</button>
+    </>
+  )
+}
+
+export default App
